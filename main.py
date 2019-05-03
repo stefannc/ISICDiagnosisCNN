Added model evaluation, some statistics for tensorboard. Fixed accuracy calculation (was wrong)

main.py
@@ -74,32 +74,21 @@ def checkPerformance(loader, model, epoch):
             for t, p in zip(y.view(-1), preds.view(-1)):
                 confusion_matrix[t.long(), p.long()] += 1
         
-        #Calculating accuracy        
-        accs = 100 * confusion_matrix.diag()/confusion_matrix.sum(1)
-        mean_accs = sum(accs) / 7
-        print(accs)
-        print('Mean:', mean_accs)
-        
         #Calculating other statistics
-        spec = []
-        sens = []
         TP = confusion_matrix.diag()
+        TN = []
+        FP = []
+        FN = []
         for c in range(7):
             idx = torch.ones(7).byte()
             idx[c] = 0
-            TN = confusion_matrix[idx.nonzero()[:, None], idx.nonzero()].sum()
-            FP = confusion_matrix[idx, c].sum()
-            FN = confusion_matrix[c, idx].sum()
-            spec.append(TN / (TN + FP))
-            sens.append(TP[c] / (TP[c] + FN))
-        mean_spec = sum(spec) / 7
-        mean_sens = sum(sens) / 7
+            TN.append(confusion_matrix[idx.nonzero()[:, None], idx.nonzero()].sum())
+            FP.append(confusion_matrix[idx, c].sum())
+            FN.append(confusion_matrix[c, idx].sum())
         
-        #Writing to tensorboard
-        writer.add_scalar('Mean Accuracy', mean_accs, epoch)
-        writer.add_scalar('Mean Sensitivity', mean_sens, epoch)
-        writer.add_scalar('Mean Specificity', mean_spec, epoch)
-        writer.close()
+        printing = True
+        tboard = True
+        supportFunctions.performance(TP, TN, FP, FN, printing, tboard, epoch = epoch, writer = writer)
 ############################
         
 ####### MAIN CODE #######
@@ -132,20 +121,22 @@ model = nn.Sequential(
 
 #Model and parameter inits
 learning_rate = 1e-4
-epochs = 5
-model = torchvision.models.resnet34(pretrained = True)
+epochs = 10
+model = torchvision.models.resnet50(pretrained = True)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 7)
-optimizer = optim.Adagrad(model.parameters(), lr = learning_rate)
+#optimizer = optim.Adagrad(model.parameters(), lr = learning_rate)
 start_epoch = 1
 
 #model.apply(initWeights)
-#optimizer = optim.SGD(model.parameters(), lr = learning_rate,
-#                      momentum = 0.9, nesterov = True)
+optimizer = optim.SGD(model.parameters(), lr = learning_rate,
+                      momentum = 0.9, weight_decay = 0,
+                      nesterov = True)
 
-load_model = 'checkpoint.pth'
+
+load_model = ''
 if load_model == '':
-    print('No pretrained loaded model found. Using default model (Resnet34)')
+    print('No pretrained loaded model found. Using default model (Resnet50)')
 else:
     model, optimizer, start_epoch = supportFunctions.loadModel(model, optimizer, load_model)
     
@@ -163,7 +154,7 @@ writer.add_image('images', grid, 0)
 writer.close()
 
 #Function calls
-trainModel(model, optimizer, train, epochs = epochs, start_epoch)
+trainModel(model, optimizer, train, epochs = epochs, start_epoch = start_epoch)
 checkPerformance(test, model, epochs + start_epoch)
 
 #Saving

supportFunctions.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def getDevice(USE_GPU): #Returns GPU if CUDA is available, else CPU
     
@@ -17,13 +18,49 @@ def saveModel(model, optimizer, epochs):
     torch.save(state, 'checkpoint.pth')
     return
 
-def loadModel(model, optimizer, filename):
-    print('Loading checkpoint from', filename)
-    checkpoint = torch.load(filename)
-    start_epoch = checkpoint['epoch']
-    model.load_state_dict(checkpoint['state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
-    print('Loaded checkpoint at epoch:', start_epoch)
+def loadModel(model, optimizer = None, filename = None):
+    if not filename == None:
+        print('Loading checkpoint from', filename)
+        checkpoint = torch.load(filename)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        if not optimizer == None:
+            optimizer.load_state_dict(checkpoint['optimizer'])
+        print('Loaded checkpoint at epoch:', start_epoch)
     
     return model, optimizer, start_epoch
-    
+
+def performance(TP, TN, FP, FN, printing, tboard, epoch = 0, writer = None):
+    class_names = ['melanoma', 'nevus', 'carcinoma', 'bowens', 'keratosis', 'dermatofibroma', 'vascular']
+    accuracy = []
+    sensitivity = []
+    specificity = []
+    precision = []
+    F1Score = []
+    for i in range(7):
+        accuracy.append(100*(TP[i] + TN[i])/(TP[i] + TN[i] + FP[i] + FN[i]))
+        sensitivity.append(100*(TP[i])/(TP[i]+FN[i]))
+        specificity.append(100*(TN[i])/(TN[i]+FP[i]))
+        precision.append(100*(TP[i])/(TP[i]+FP[i]))
+        F1Score.append((2*TP[i])/(2*TP[i] + FP[i] + FN[i]))
+        if printing:
+            print('########################################################')
+            print(class_names[i])
+            print('Accuracy:', accuracy[i])
+            print('Sensitivity/Recall:', sensitivity[i])
+            print('Specificity', specificity[i])
+            print('Precision', precision[i])
+            print('F1 Score', F1Score[i])
+            print('########################################################')
+
+    if tboard:
+        m_acc = np.sum(accuracy)/7
+        m_rec = np.sum(sensitivity)/7
+        m_pre = np.sum(precision)/7
+        m_f1 = np.sum(F1Score)/7
+        writer.add_scalar('Accuracy', m_acc, epoch)
+        writer.add_scalar('Recall', m_rec, epoch)
+        writer.add_scalar('Precision', m_pre, epoch)
+        writer.add_scalar('F1-Score', m_f1, epoch)
+        writer.close()
+